18f6bea92b59124fcad00a98752c165e10d2179a
Özer <83964758+cbr00t@users.noreply.github.com>
2024-11-05 14:59:45 +0300

    Toplu düzenlemeler: - Alt Struct sınıfları ilgili Req/Resp CS dosyası içine taşındı - CRRMR_GYPerSozlesmeliTotalPuantajOlusturResult: (eksikNedenKod, eksikGun) tekil verileri yerine 'eksikBilgileri[]' eklendi

--- src/rrm/modul/guleryuz/personel/structs/tables/project/CRRMR_GYPerSozlesmeliTotalPuantajOlusturArg.cs
+++ src/rrm/modul/guleryuz/personel/structs/tables/project/CRRMR_GYPerSozlesmeliTotalPuantajOlusturArg.cs
@@ -9,40 +9,21 @@ using System.ComponentModel;
 namespace CVioWebServis {
     [Serializable()]
     public class CRRMR_GYPerSozlesmeliTotalPuantajOlusturArg : CRRMRequestRecord {
-        #region Accessors
-        [XmlElement( "perID" )]
-        public Guid PerID { get; set; }
-
-        [XmlElement( "donemID" )]
-        public Guid DonemID { get; set; }
-
-        [XmlElement( "yilAy" ), DefaultValue( 0 )]
-        public int YilAy { get; set; }
-
-        [XmlElement( "yil" ), DefaultValue( 0 )]
-        public int Yil { get; set; }
-
-        [XmlElement( "ay" ), DefaultValue( 0 )]
-        public int Ay { get; set; }
-
-        [XmlElement( "eksikNedenKod" )]
-        public string EksikNedenKod { get; set; }
-
-        [XmlElement( "eksikGun" ), DefaultValue( 0 )]
-        public int EksikGun { get; set; }
-
-        [XmlElement( "eksikSaat" ), DefaultValue( typeof( decimal ), "0" )]
-        public decimal EksikSaat { get; set; }
-
-        [XmlElement( "sskGunu" ), DefaultValue( 0 )]
-        public int SSKGunu { get; set; }
-
-        [XmlElement( "vergiGunu" ), DefaultValue( 0 )]
-        public int VergiGunu { get; set; }
-
-        [XmlArray( "odemeVerileri" )]
-        [XmlArrayItem( "odeme" )]
-        public CRRMR_GYPerSTP_OdemeVerileri[] OdemeVerileri { get; set; }
-        #endregion
+        [XmlElement("perID")] public Guid PerID { get; set; }
+        [XmlElement("donemID")] public Guid DonemID { get; set; }
+        [XmlElement("yilAy"), DefaultValue(0)] public int YilAy { get; set; }
+        [XmlElement("yil"), DefaultValue(0)] public int Yil { get; set; }
+        [XmlElement("ay"), DefaultValue(0)] public int Ay { get; set; }
+        [XmlElement("eksikNedenKod")] public string EksikNedenKod { get; set; }
+        [XmlElement("eksikGun"), DefaultValue(0)] public int EksikGun { get; set; }
+        [XmlElement("eksikSaat"), DefaultValue(typeof(decimal), "0")] public decimal EksikSaat { get; set; }
+        [XmlElement("sskGunu"), DefaultValue(0)] public int SSKGunu { get; set; }
+        [XmlElement("vergiGunu"), DefaultValue(0)] public int VergiGunu { get; set; }
+        [XmlArray("odemeVerileri"), XmlArrayItem("odeme")] public CRRMR_GYPerSTP_OdemeVerileri[] OdemeVerileri { get; set; }
     }
+	[Serializable()]
+	public class CRRMR_GYPerSTP_OdemeVerileri : CRRMRecord {
+		[XmlElement("odemeKod")] public string OdemeKod { get; set; }
+		[XmlElement("veri"), DefaultValue(typeof(decimal), "0")] public decimal Veri { get; set; }
+	}
 }
--- src/rrm/modul/guleryuz/personel/structs/tables/project/CRRMR_GYPerSozlesmeliTotalPuantajOlusturResult.cs
+++ src/rrm/modul/guleryuz/personel/structs/tables/project/CRRMR_GYPerSozlesmeliTotalPuantajOlusturResult.cs
@@ -9,36 +9,18 @@ using System.ComponentModel;
 namespace CVioWebServis {
     [Serializable()]
     public class CRRMR_GYPerSozlesmeliTotalPuantajOlusturResult : CRRMResponseRecord {
-        #region Accessors
-        [XmlElement( "yilAy" ), DefaultValue( 0 )]
-        public int YilAy { get; set; }
-
-        [XmlElement( "yil" ), DefaultValue( 0 )]
-        public int Yil { get; set; }
-
-        [XmlElement( "ay" ), DefaultValue( 0 )]
-        public int Ay { get; set; }
-
-        [XmlElement( "goPerID" )]
-        public Guid? GoPerID { get; set; }
-
-        [XmlElement( "perSayac" ), DefaultValue( 0 )]
-        public int PerSayac { get; set; }
-
-        [XmlElement( "perKod" )]
-        public string PerKod { get; set; }
-
-        /*[XmlElement( "perIsim" )]
-        public string PerIsim { get; set; }*/
-
-        [XmlElement( "calismaID" ), DefaultValue( 0 )]
-        public int CalismaID { get; set; }
-
-        [XmlElement( "eksikNedenKod" )]
-        public string EksikNedenKod { get; set; }
-
-        [XmlElement( "eksikGun" ), DefaultValue( 0 )]
-        public int EksikGun { get; set; }
-        #endregion
+        [XmlElement("yilAy"), DefaultValue(0)] public int YilAy { get; set; }
+        [XmlElement("yil"), DefaultValue(0)] public int Yil { get; set; }
+        [XmlElement("ay"), DefaultValue(0)] public int Ay { get; set; }
+        [XmlElement("goPerID")] public Guid? GoPerID { get; set; }
+        [XmlElement("perSayac"), DefaultValue(0)] public int PerSayac { get; set; }
+        [XmlElement("perKod")] public string PerKod { get; set; }
+        /*[XmlElement("perIsim")] public string PerIsim { get; set; }*/
+        [XmlElement("calismaID"), DefaultValue(0)] public int CalismaID { get; set; }
+		[XmlArray("eksikBilgileri"), XmlArrayItem("eksikBilgi")] public CRRMR_GYPer_EksikBilgi[] EksikBilgileri { get; set; }
     }
+    [Serializable()] public class CRRMR_GYPer_EksikBilgi : CRRMRecord {
+		[XmlElement("nedenKod")] public string NedenKod { get; set; }
+		[XmlElement("gun"), DefaultValue(0)] public decimal Gun { get; set; }
+	}
 }

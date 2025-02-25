c09abf275e9596c04d53fd640bef561020e133a7
Özer <83964758+cbr00t@users.noreply.github.com>
2025-02-25 22:16:56 +0300

    CRRMR_GYPerTarFirmaMaliyet::fazlaMesaiTumNet

--- src/rrm/modul/guleryuz/personel/structs/tables/project/CRRMR_GYPerTarFirmaMaliyet.cs
+++ src/rrm/modul/guleryuz/personel/structs/tables/project/CRRMR_GYPerTarFirmaMaliyet.cs
@@ -9,7 +9,6 @@ using System.ComponentModel;
 namespace CVioWebServis {
     [Serializable()]
     public class CRRMR_GYPerTarFirmaMaliyet : CRRMResponseRecord {
-        #region Accessors
         [XmlElement("goPerID")] public Guid PerID { get; set; }
         [XmlElement("perIsim")] public string PerIsim { get; set; }
         [XmlElement("tckimlikno")] public string TCKimlikNo { get; set; }
@@ -23,7 +22,8 @@ namespace CVioWebServis {
         [XmlElement("normalOdeme"), DefaultValue(typeof(decimal), "0")] public decimal NormalOdeme { get; set; }
         [XmlElement("fazlaMesai"), DefaultValue(typeof(decimal), "0")] public decimal FazlaMesai { get; set; }
         [XmlElement("fazlaMesaiSaat"), DefaultValue(typeof(decimal), "0")] public decimal FazlaMesaiSaat { get; set; }
-        [XmlElement("calismaGunu"), DefaultValue(typeof(decimal), "0")] public decimal CalismaGunu { get; set; }
+		[XmlElement("fazlaMesaiTumNet"), DefaultValue(typeof(decimal), "0")] public decimal FazlaMesaiTumNet { get; set; }
+		[XmlElement("calismaGunu"), DefaultValue(typeof(decimal), "0")] public decimal CalismaGunu { get; set; }
         [XmlElement("yanOdeme"), DefaultValue(typeof(decimal), "0")] public decimal YanOdeme { get; set; }
         [XmlElement("ozelKesinti"), DefaultValue(typeof(decimal), "0")] public decimal OzelKesinti { get; set; }
         [XmlElement("isverenPrimi"), DefaultValue(typeof(decimal), "0")] public decimal IsverenPrimi { get; set; }
@@ -41,18 +41,10 @@ namespace CVioWebServis {
             get {
                 if (SSKDurumKod != null) {
                     switch (SSKDurumKod.ToUpper()) {
-                        case "E": return CRRM_SSKDurum.Emekli;
-                        case "C": return CRRM_SSKDurum.Cirak;
-                        case "S": return CRRM_SSKDurum.Stajyer;
-                        case "Y": return CRRM_SSKDurum.Yabanci_AnlasmaliUlke;
-                        case "Z": return CRRM_SSKDurum.Yabanci_DigerUlke;
-                        case "D": return CRRM_SSKDurum.YurtDisindaCalisan;
-                        case "F6": return CRRM_SSKDurum.FiiliHizmetZammi_60Gun;
-                        case "F9": return CRRM_SSKDurum.FiiliHizmetZammi_90Gun;
-                        case "F8": return CRRM_SSKDurum.FiiliHizmetZammi_180Gun;
-                        case "H": return CRRM_SSKDurum.HakkiHuzur;
-                        case "HM": return CRRM_SSKDurum.HarpMalulu;
-                        case "KM": return CRRM_SSKDurum.KamuIhaleKanunuIstihdami;
+                        case "E": return CRRM_SSKDurum.Emekli; case "C": return CRRM_SSKDurum.Cirak; case "S": return CRRM_SSKDurum.Stajyer;
+                        case "Y": return CRRM_SSKDurum.Yabanci_AnlasmaliUlke; case "Z": return CRRM_SSKDurum.Yabanci_DigerUlke; case "D": return CRRM_SSKDurum.YurtDisindaCalisan;
+                        case "F6": return CRRM_SSKDurum.FiiliHizmetZammi_60Gun; case "F9": return CRRM_SSKDurum.FiiliHizmetZammi_90Gun; case "F8": return CRRM_SSKDurum.FiiliHizmetZammi_180Gun;
+                        case "H": return CRRM_SSKDurum.HakkiHuzur; case "HM": return CRRM_SSKDurum.HarpMalulu; case "KM": return CRRM_SSKDurum.KamuIhaleKanunuIstihdami;
                         case "R": return CRRM_SSKDurum.Diger;
                     }
                 }
@@ -79,6 +71,5 @@ namespace CVioWebServis {
         }
         [XmlElement("agiDahilNet"), DefaultValue(typeof(decimal), "0")] public decimal AgiDahilNet { get; set; }
         [XmlElement("firmaMaliyet"), DefaultValue(typeof(decimal), "0")] public decimal FirmaMaliyet { get; set; }
-        #endregion
     }
 }
